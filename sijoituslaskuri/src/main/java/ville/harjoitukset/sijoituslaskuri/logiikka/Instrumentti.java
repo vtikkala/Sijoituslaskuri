41a4b92e4448fc7fe2fa50ec99c5539f91280205
Ville Tikkala <ville.tikkala@helsinki.fi>
2016-11-18 00:21:27 +0200

    Päivitetty luokkia

--- sijoituslaskuri/src/main/java/ville/harjoitukset/sijoituslaskuri/logiikka/Instrumentti.java
+++ sijoituslaskuri/src/main/java/ville/harjoitukset/sijoituslaskuri/logiikka/Instrumentti.java
@@ -5,18 +5,14 @@ package ville.harjoitukset.sijoituslaskuri.logiikka;
  * @author Ville Tikkala
  */
 public abstract class Instrumentti {
+//    public Parametrit parametrit; // Sijoituskohteen laskentaparametrit
     private String tunnus; // Sijoituskohteen luokittelu
     private String nimi; // Sijoituskohteen nimi
-    private String omaisuusluokka; // Sijoituskohteen omaisuusluokka
-    private double tuotto; // Sijoituskohteen tuotto-oletus
     private double osuus; // Sijoituskohteen osuus koko portfoliossa (%) POISTETAAN TÄÄLTÄ!
     
-    public Instrumentti(String tunnus, String nimi, String luokka, double tuotto, double osuus) {
+    public Instrumentti(String tunnus, String nimi) {           
         this.tunnus = tunnus;
         this.nimi = nimi;
-        this.omaisuusluokka = luokka;
-        this.tuotto = tuotto;
-        this.osuus = osuus;
     }
     
     public boolean setTunnus(String tunnus) {
@@ -37,24 +33,6 @@ public abstract class Instrumentti {
         return this.nimi;
     }
     
-    public boolean setOmaisuusluokka(String luokka) {
-        this.omaisuusluokka = luokka;
-        return true;
-    }
-    
-    public String getOmaisuusluokka() {
-        return this.omaisuusluokka;
-    }
-    
-    public boolean setTuotto(double tuotto) {
-        this.tuotto = tuotto;
-        return true;
-    }
-    
-    public double getTuotto() {
-        return this.tuotto;
-    }
-    
     public boolean setOsuus(double osuus) {
         this.osuus = osuus;
         return true;
@@ -63,5 +41,6 @@ public abstract class Instrumentti {
     public double getOsuus() {
         return this.osuus;
     }
+    
     
 }
--- sijoituslaskuri/src/main/java/ville/harjoitukset/sijoituslaskuri/logiikka/Parametrit.java
+++ sijoituslaskuri/src/main/java/ville/harjoitukset/sijoituslaskuri/logiikka/Parametrit.java
@@ -1,14 +1,75 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ville.harjoitukset.sijoituslaskuri.logiikka;
 
 /**
  *
- * @author yanis
+ * @author Ville Tikkala
  */
 public class Parametrit {
+    private int omaisuusluokka; // pakollinen tieto
+    private int rahastotyyppi; // (1 = kasvu, 2 = tuotto)
+    private double tuotto; // pakollinen tieto
+    private double merkintapalkkio;
+    private double lunastuspalkkio;
+    private double hallinnointipalkkio;
+    
+    public Parametrit(int luokka, double tuotto) {
+        this.omaisuusluokka = luokka;
+        this.tuotto = tuotto;
+    }
+    
+    public Parametrit(int luokka, int rahastotyyppi, double tuotto, double merkintapalkkio,
+            double lunastuspalkkio, double hallinnointipalkkio) {
+        this.omaisuusluokka = luokka;
+        this.rahastotyyppi = rahastotyyppi;
+        this.tuotto = tuotto;
+        this.merkintapalkkio = merkintapalkkio;
+        this.lunastuspalkkio = lunastuspalkkio;
+        this.hallinnointipalkkio = hallinnointipalkkio;
+    }
+    
+    public boolean setOmaisuusluokka(int luokka) {
+        this.omaisuusluokka = luokka;
+        return true;
+    }
+    
+    public int getOmaisuusluokka() {
+        return this.omaisuusluokka;
+    }
+    
+    public boolean setTuotto(double tuotto) {
+        this.tuotto = tuotto;
+        return true;
+    }
+    
+    public double getTuotto() {
+        return this.tuotto;
+    }
+    
+    public boolean setMerkintapalkkio(double merkintapalkkio) {
+        this.merkintapalkkio = merkintapalkkio;
+        return true;
+    }
+    
+    public double getMerkintapalkkio() {
+        return this.merkintapalkkio;
+    }
+    
+    public boolean setLunastuspalkkio(double lunastuspalkkio) {
+        this.lunastuspalkkio = lunastuspalkkio;
+        return true;
+    }
+    
+    public double getLunastuspalkkio() {
+        return this.lunastuspalkkio;
+    }
+    
+    public boolean setHallinnointipalkkio(double hallinnointipalkkio) {
+        this.hallinnointipalkkio = hallinnointipalkkio;
+        return true;
+    }
+    
+    public double getHallinnointipalkkio() {
+        return this.hallinnointipalkkio;
+    }
     
 }
